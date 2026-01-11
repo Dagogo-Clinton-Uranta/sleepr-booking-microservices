47f1f9078932197ae62f9070b1d2040bf3d58063
Dagogo-Clinton-Uranta <dagogouranta@gmail.com>
2026-01-11 15:59:43 +0100

    fix useFactory method in database module in libs

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import { ConfigService, ConfigModule as NestConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
     //for root is sort of like an initializer fir whatever we installed
     //in this case, for root, is telling nest js to load enviromnent variables from memory and read  dot envfiles
-    imports: [NestConfigModule.forRoot()] //we are setting our db in our config module here, for easy swappability
-
+    imports: [NestConfigModule.forRoot({
+        validationSchema:Joi.object({
+            MONGODB_URI:Joi.string().required(), //validatin that mongoDB is a string
+        })
+    })] ,//we are setting our db in our config module here, for easy swappability
+    providers: [ConfigService],
+    exports: [ConfigService],
+   
 
 })
 export class ConfigModule {
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
     //for root is sort of like an ainitializer
-    imports: [MongooseModule.forRoot(''/**connection string to be here */)]
+    imports: [MongooseModule.forRootAsync({
+        imports:[ConfigModule], //we cant use configService below without importing the ConfigModule, which houses the configService
+         useFactory:(configService:ConfigService)=>({
+       //configService above is using .env internally
+        uri: configService.get('MONGODB_URI') /**connection string to be here */
+      
+    }),
+      inject:[ConfigService] //the list of dependecies we need available to actually run this useFactory function
+    
+    
+    }),
+
+],
+ //we changed from forRoot to forRootAsync - forRootAsync allows us to inject depencies into a factory method
+ //and that means we will get access to config service - i dont understand how using a factory methos, allows access to the config service
+
 })
 export class DatabaseModule {}
