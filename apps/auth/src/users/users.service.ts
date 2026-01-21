02723113097aba4cf469497a478db3417e199eb5
Dagogo-Clinton-Uranta <dagogouranta@gmail.com>
2026-01-21 09:55:09 +0100

    setting up the database module in the users microservice

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-
+ 
+    constructor(private readonly usersRepository:UsersRepository){
+        
+    }
 
     async create(createUserDto: CreateUserDto){
-
+     return this.usersRepository.create(createUserDto);
     }
 }
 
